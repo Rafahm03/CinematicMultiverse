1b5724cf17ca82d5652dcde61ca1d060e8c5625b
Rhm03 <rafahernandezmelendez@gmail.com>
2025-02-18 19:48:48 +0100

    initial commit

---
+++ CinematicMultiverse/src/main/java/com/example/CinematicMultiverse/CinematicMultiverseApplication.java
@@ -0,0 +1,13 @@
+package com.example.CinematicMultiverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinematicMultiverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinematicMultiverseApplication.class, args);
+	}
+
+}
---
+++ CinematicMultiverse/src/test/java/com/example/CinematicMultiverse/CinematicMultiverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CinematicMultiverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinematicMultiverseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
