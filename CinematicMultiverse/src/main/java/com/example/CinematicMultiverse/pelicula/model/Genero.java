efab8dd950a9a584b5f285d4a5caad41c77a5448
Rhm03 <rafahernandezmelendez@gmail.com>
2025-02-22 23:03:47 +0100

    se creo la entidad pelicula con sus generos, ademas se añadieron datos de prubas

---
+++ CinematicMultiverse/src/main/java/com/example/CinematicMultiverse/pelicula/model/Genero.java
@@ -0,0 +1,17 @@
+package com.example.CinematicMultiverse.pelicula.model;
+
+public enum Genero {
+    ACCION,
+    FANTASIA,
+    CRIMEN,
+    BELICO,
+    ROMANCE,
+    DRAMA,
+    CIENCIA_FICCION,
+    COMEDIA,
+    TERROR,
+    MUSICAL,
+    DOCUMENTAL,
+    ANIME,
+    AVENTURA;
+}
