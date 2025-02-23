0e1b7efab82530b539f091256b73d9f179e6b415
Rhm03 <rafahernandezmelendez@gmail.com>
2025-02-23 12:45:22 +0100

    Se creo el endpoint de listar las peliculas correctamente

--- CinematicMultiverse/src/main/java/com/example/CinematicMultiverse/pelicula/service/PeliculaService.java
+++ CinematicMultiverse/src/main/java/com/example/CinematicMultiverse/pelicula/service/PeliculaService.java
@@ -1,13 +1,17 @@
 package com.example.CinematicMultiverse.pelicula.service;
 
 import com.example.CinematicMultiverse.pelicula.dto.EditPeliculaCmd;
+import com.example.CinematicMultiverse.pelicula.dto.GetPeliculaDto;
+import com.example.CinematicMultiverse.pelicula.error.PeliculaNotFoundException;
 import com.example.CinematicMultiverse.pelicula.model.Genero;
 import com.example.CinematicMultiverse.pelicula.model.Pelicula;
 import com.example.CinematicMultiverse.pelicula.repo.PeliculaRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -16,6 +20,21 @@ import java.util.stream.Collectors;
 public class PeliculaService {
 
     private final PeliculaRepository peliculaRepository;
+
+
+    @Transactional
+    public List<GetPeliculaDto  > findAll(){
+
+        List<Pelicula> result = peliculaRepository.findAll();
+
+        List<GetPeliculaDto> result2 = result.stream().map(GetPeliculaDto::of).toList();
+
+        if (result2.isEmpty()){
+            throw new PeliculaNotFoundException("No existen películas");
+        }
+        return result2;
+    }
+
 
     public Pelicula save(EditPeliculaCmd editPeliculaCmd) {
         Set<Genero> generos = Arrays.stream(editPeliculaCmd.genero().split(","))
